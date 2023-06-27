2912f5201ece059ca1eb16089d7228010dace83e
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-27 20:58:42 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -2,3 +2,5 @@
 #set -o allexport; source .env; set +o allexport;
 
 
+mkdir -p storage/media storage/caddy_condig storage/caddy_data
+chown 1000:82 storage/media storage/caddy_condig storage/caddy_data
