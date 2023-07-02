20d5a3d063e21560da634a4d63d5416fc12844f4
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-07-02 16:10:42 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -7,4 +7,6 @@ rm docker-compose.prod.yml
 rm docker-compose.yml
 sed -i "s~COPY package-lock.json \$KBIN_HOME~# COPY package-lock.json \$KBIN_HOME~g" ./Dockerfile
 
+sed -i "s~router.request_context.scheme: https~router.request_context.scheme: https \ asset.request_context.secure: true~g" ./config/services.yaml
+
 docker buildx build . --output type=docker,name=elestio4test/kbin:latest | docker load
