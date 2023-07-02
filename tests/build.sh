4b7d1fec636d5876b53f9ba8f937a931da0bad3a
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-07-02 15:07:31 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,20 +1,20 @@
 pwd=$(pwd)
 
-cp $pwd/docker/v2/* $pwd
+cp -R $pwd/docker/v2/* $pwd
 
 rm docker-compose.override.yml
 rm docker-compose.prod.yml
 rm docker-compose.yml
 
 # sed -i "s~--link ~~g" ./Dockerfile
-# sed -i "s~COPY package-lock.json \$KBIN_HOME~# COPY package-lock.json \$KBIN_HOME~g" ./Dockerfile
-sed -i "s~COPY --chmod=755 docker/v2/docker-entrypoint ./~COPY docker/v2/docker-entrypoint ./~g" ./Dockerfile
+sed -i "s~COPY package-lock.json \$KBIN_HOME~# COPY package-lock.json \$KBIN_HOME~g" ./Dockerfile
+# sed -i "s~COPY --chmod=755 docker/v2/docker-entrypoint ./~COPY docker/v2/docker-entrypoint ./~g" ./Dockerfile
 # sed -i "s~COPY composer.* \$KBIN_HOME~COPY composer.* \$KBIN_HOME/~g" ./Dockerfile
 
 # chmod 755 ./docker-entrypoint
 
-docker build . --tag elestio4test/kbin:latest
-docker buildx build . --tag elestio4test/kbin:latest --output type=local
+# docker build . --tag elestio4test/kbin:latest
+docker buildx build . --output type=docker,name=elestio4test/kbin:latestu | docker load
 
 # cp $pwd/docker/v2/Dockerfile $pwd
 # sed -i "s~COPY package-lock.json \$KBIN_HOME~# COPY package-lock.json \$KBIN_HOME~g" ./Dockerfile
