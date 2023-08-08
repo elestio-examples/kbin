f30b3511a6852edf3eaf7deeec664eb66361d265
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2023-08-08 21:04:45 +0530

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -6,6 +6,7 @@ rm docker-compose.override.yml
 rm docker-compose.prod.yml
 rm docker-compose.yml
 sed -i "s~COPY package-lock.json \$KBIN_HOME~# COPY package-lock.json \$KBIN_HOME~g" ./Dockerfile
+sed -i 's~COPY --chown=$USER:$GROUP --link ./ $KBIN_HOME~COPY --chown=$USER:$GROUP ./ $KBIN_HOME~g' ./Dockerfile
 sed -i '/router.request_context.scheme: https/a \  asset.request_context.secure: true' ./config/services.yaml
 
 docker buildx build . --output type=docker,name=elestio4test/kbin:latest | docker load
